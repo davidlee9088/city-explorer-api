965ac3faa9a0b2672d2104ddd006c44e7be2885c
Hanbyeol <davidlee9088@gmail.com>
2022-08-25 20:07:48 -0700

    need help with lab8

--- server.js
+++ server.js
@@ -2,10 +2,9 @@
 
 console.log('Our frist server');
 
-require('dotenv').config(); // makes it possible to go in  dotenv files and get the port
-
-const express = require('express'); // requiring express library lin
-const cors = require('cors'); // require imports cors allows share info, middleware
+require('dotenv').config(); 
+const express = require('express'); 
+const cors = require('cors'); 
 const app = express();
 const weatherData = require('./data/weather.json');
 app.use(cors());
@@ -16,20 +15,20 @@ const PORT = process.env.PORT || 3002; //define port
 
 app.get('/weatherData', (request, response) => {
   const searchQuery = request.query.searchQuery;
-  //console.log('searchQuery', searchQuery);
+
   console.log(searchQuery);
 
   let searchResult = weatherData.find(object => object.city_name === searchQuery);
   console.log(searchResult);
 
-  const result = searchResult.data.map(dayObj => new Forecast(dayObj));
-  //const result = new Forecast(searchResult); original
-  response.status(200).send(result); // send it back to original request
-  //console.log(result[0].data); first mod
+  const result = searchResult.data.map(dayObj => new Cast(dayObj));
+ 
+  response.status(200).send(result); 
+ 
   console.log(result);
 });
 
-class Forecast {
+class Cast {
   constructor(weatherObject) {
     this.datetime = weatherObject.datetime;
     this.description = weatherObject.weather.description;
